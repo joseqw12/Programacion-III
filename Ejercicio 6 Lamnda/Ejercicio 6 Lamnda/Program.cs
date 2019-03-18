685cb2d913ef238fc1ba10d0158723a0ed042857
joseqw12 <josedieter12@gmail.com>
2019-03-17 22:41:53 -0400

    Tarea ejem6

---
+++ Ejercicio 6 Lamnda/Ejercicio 6 Lamnda/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio_6_Lamnda
+{
+    
+    class Program
+    {
+        delegate int operaciones(int x, int y);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("valores para X=10 y Y=2");
+            
+            operaciones del = (int x, int y) =>
+            { return x + y; };
+            Console.WriteLine("Suma");
+            
+            Console.WriteLine(del(10,2 ));
+
+
+            del = (int a, int b) =>
+            { return a - b; };
+
+            Console.WriteLine("Reta");
+            Console.WriteLine(del(10, 2));
+
+
+            del = (int a, int b) =>
+            { return a * b; };
+            Console.WriteLine("Multiplicaion");
+            Console.WriteLine(del(10, 2));
+
+            del = (int a, int b) =>
+            { return a /b; };
+            Console.WriteLine("Division");
+            Console.WriteLine(del(10, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
